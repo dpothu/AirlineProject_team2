5d92242f6d050a4a407695b6e7c9c4e3f6b1b2e1
ProfessorKTS <122438560+ProfessorKTS@users.noreply.github.com>
2023-01-31 15:37:55 +0530

    Delete BookingService_31_JAN directory

--- BookingService_31_JAN/src/main/java/com/bookingservice/exception/FareNotPresent.java
+++
@@ -1,9 +0,0 @@
-package com.bookingservice.exception;
-
-@SuppressWarnings("serial")
-public class FareNotPresent extends RuntimeException{
-
-	public FareNotPresent(String s) {
-		super(String.format(s));
-	}
-}
