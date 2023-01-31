f621a97bd2a49cf0e4d4ac17e845f1f98d661138
ProfessorKTS <122438560+ProfessorKTS@users.noreply.github.com>
2023-01-31 16:07:02 +0530

    Update InventoryServiceImpl.java

--- src/main/java/com/bookingservice/service/Impl/InventoryServiceImpl.java
+++ src/main/java/com/bookingservice/service/Impl/InventoryServiceImpl.java
@@ -53,7 +53,7 @@ public class InventoryServiceImpl implements InventoryService{
 		
 		logger.info("Inventory Service implementation setDataToInventory starts: ");
 		Random random = new Random();
-		long pnrLong = random.nextLong(999999, 9999999);
+		long pnrLong = random.nextLong();
 		String pnrString = Long.toString(pnrLong);
 		inven.setFlightId(flightDetails.getId());
 		inven.setFlightName(flightDetails.getName());
