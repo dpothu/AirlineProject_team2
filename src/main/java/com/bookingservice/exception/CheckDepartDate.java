2b7740ee22ead2ef04a2bb400edaad41a962f657
dpothu <122075632+dpothu@users.noreply.github.com>
2023-02-02 16:25:01 +0530

    Delete src directory

--- src/main/java/com/bookingservice/exception/CheckDepartDate.java
+++
@@ -1,9 +0,0 @@
-package com.bookingservice.exception;
-
-@SuppressWarnings("serial")
-public class CheckDepartDate extends RuntimeException{
-
-	public CheckDepartDate() {
-		super(String.format("Departure cannot be before current date."));
-	}
-}
